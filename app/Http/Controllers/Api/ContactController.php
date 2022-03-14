8e78b6e00e8b71ef9b96b2c88384dc3fba33e19b
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-03-14 17:37:20 +0100

    add new contacts in db

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -5,6 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Validator;
+use App\Lead;
+use App\Mail\SendNewMail;
+use App\Mail\SendNewMailGuest;
+use Illuminate\Support\Facades\Mail;
+
 class ContactController extends Controller
 {
     public function store (Request $request){
